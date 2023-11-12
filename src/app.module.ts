5551b6a40666696f4e3f3ad0295e1293e7fb9891
Mykyta Kasianenko <kasianenkomykyta@gmail.com>
2023-11-12 12:50:51 +0200

    refactor: moved database config in own module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -11,20 +11,8 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
     }),
     HealthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      password: process.env.DB_PASSWORD,
-      username: process.env.DB_USER,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      migrations: [__dirname + '/migrations/*{.ts,.js}'],
-      migrationsTableName: 'api_migrations_typeorm',
-      database: process.env.DB_NAME,
-      synchronize: true,
-      logging: true,
-    }),
     UsersModule,
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
